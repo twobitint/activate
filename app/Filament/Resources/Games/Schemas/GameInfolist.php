98a4dce5f6ba3319a66af18df313b082ddfb6d12
Justin Labak <jtlabak@gmail.com>
2026-01-19 19:31:04 -0800

    fix another namespace

--- app/Filament/Resources/Games/Schemas/GameInfolist.php
+++ app/Filament/Resources/Games/Schemas/GameInfolist.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Filament\Resources\Rooms\Schemas;
+namespace App\Filament\Resources\Games\Schemas;
 
 use Filament\Infolists\Components\TextEntry;
 use Filament\Schemas\Schema;
