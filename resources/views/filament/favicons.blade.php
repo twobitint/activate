ff4facf7e0a25c63eabbce9bff99743c740b03fc
Justin Labak <jtlabak@gmail.com>
2026-01-20 19:30:08 -0800

    add icon 192

--- resources/views/filament/favicons.blade.php
+++ resources/views/filament/favicons.blade.php
@@ -1,3 +1,4 @@
 <link rel="apple-touch-icon" sizes="16x16" href="/favicon_16x16.png">
 <link rel="apple-touch-icon" sizes="32x32" href="/favicon_32x32.png">
 <link rel="apple-touch-icon" sizes="96x96" href="/favicon_96x96.png">
+<link rel="apple-touch-icon" sizes="192x192" href="/favicon_192x192.png">
